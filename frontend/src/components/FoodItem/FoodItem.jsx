1c55b1719b33757587269a034ab8e8a7ceb35e0d
Your Name <habibseid2829@gmail.com>
2026-07-06 15:45:53 -0700

    Implement item counter functionality in FoodItem component with styling

--- frontend/src/components/FoodItem/FoodItem.jsx
+++ frontend/src/components/FoodItem/FoodItem.jsx
@@ -1,12 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
 import "./FoodItem.css";
 import { assets } from "../../assets/assets";
 
 const FoodItem = ({ id, name, price, description, image }) => {
+  const [itemCount, setItemCount] = useState(0);
+
   return (
     <div className="food-item">
       <div className="food-item-img-container">
         <img src={image} alt="Food-image" className="food-item-image" />
+        {!itemCount ? (
+          <img
+            className="add"
+            src={assets.add_icon_white}
+            onClick={() => setItemCount((prev) => prev + 1)}
+          />
+        ) : (
+          <div className="food-item-counter">
+            <img
+              onClick={() => setItemCount((prev) => prev - 1)}
+              src={assets.remove_icon_red}
+              alt=""
+            />
+            <p>{itemCount}</p>
+            <img
+              onClick={() => setItemCount((prev) => prev + 1)}
+              src={assets.add_icon_green}
+              alt=""
+            />
+          </div>
+        )}
       </div>
       <div className="food-item-info">
         <div className="food-item-name-rating">
