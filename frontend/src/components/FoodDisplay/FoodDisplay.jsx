c570f6b3810bed6b12866ed8827d8a4a55fee2ee
Your Name <habibseid2829@gmail.com>
2026-08-02 02:59:39 -0700

    Pass category prop to FoodDisplay component for filtering food items

--- frontend/src/components/FoodDisplay/FoodDisplay.jsx
+++ frontend/src/components/FoodDisplay/FoodDisplay.jsx
@@ -4,23 +4,25 @@ import { useContext } from "react";
 import { StoreContext } from "../../context/StoreContext";
 import FoodItem from "../FoodItem/FoodItem";
 
-const FoodDisplay = () => {
+const FoodDisplay = ({ category }) => {
   const { food_list } = useContext(StoreContext);
   return (
     <div className="food-display" id="food-display">
       <h2>Our top dishes that you can order</h2>
       <div className="food-display-list">
         {food_list.map((item, index) => {
-          return (
-            <FoodItem
-              key={index}
-              id={item._id}
-              name={item.name}
-              description={item.description}
-              image={item.image}
-              price={item.price}
-            />
-          );
+          if (category === "All" || category === item.category) {
+            return (
+              <FoodItem
+                key={index}
+                id={item._id}
+                name={item.name}
+                description={item.description}
+                image={item.image}
+                price={item.price}
+              />
+            );
+          }
         })}
       </div>
     </div>
--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -10,7 +10,7 @@ const Home = () => {
     <div>
       <Header />
       <ExplorMenu category={category} setCategory={setCategory} />
-      <FoodDisplay />
+      <FoodDisplay category={category} />
     </div>
   );
 };
